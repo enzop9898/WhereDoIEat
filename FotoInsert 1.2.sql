02af1192a552a170759e2aeb770e68837b1cbac6
tanucc9 <maurogaetano997@gmail.com>
2019-12-23 11:38:08 +0100

    Create FotoInsert 1.2.sql

---
+++ FotoInsert 1.2.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM wheredoieat.foto
+LIMIT 0, 1000
+
+-- Date: 2019-12-23 11:37
+*/
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/bruschetta.jpg',1);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/ristorante-da-kico.jpg',1);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/img-ristoranti.jpg',2);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/la-sala.jpg',2);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/pizza.jpg',2);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/ristorante-servizio.jpg',2);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/testata-ristorante.jpg',2);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/imagesILV4I6JR.jpg',3);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/Montagna-Verde-Ristorante.jpg',3);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/ristorante-firenze.jpg',3);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/ristorante.jpg',3);
+INSERT INTO `` (`foto`,`attivitaIDattivita`) VALUES ('fotodb/ristorante1.jpg',3);
