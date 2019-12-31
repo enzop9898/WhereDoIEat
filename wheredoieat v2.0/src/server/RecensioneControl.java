816d1db9b1f09af69ee257e6efdd12ff1709e96c
tanucc9 <maurogaetano997@gmail.com>
2019-12-31 01:07:49 +0100

    wheredoieat 2.0

---
+++ wheredoieat v2.0/src/server/RecensioneControl.java
@@ -0,0 +1,77 @@
+package server;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.AttivitaBean;
+import bean.PersonaBean;
+import bean.RecensioneBean;
+import dao.AttivitaDAO;
+import dao.RecensioneDAO;
+
+/**
+ * Servlet implementation class RecensioneControl
+ */
+@WebServlet("/RecensioneControl")
+public class RecensioneControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecensioneControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PersonaBean p = (PersonaBean) request.getSession().getAttribute("cliente");
+		if(p==null)
+			request.getRequestDispatcher("/login.jsp").forward(request,response);
+		else 
+		{
+		String vals = request.getParameter("c4l-rating");
+		int val = Integer.parseInt(vals);
+		
+		String commento = (String) request.getParameter("commento");
+		String idAtts = request.getParameter("attivita");
+		
+		int idAtt = Integer.parseInt(idAtts);
+		String idP = p.getUsername();
+		
+		RecensioneDAO rd = new RecensioneDAO();
+        RecensioneBean recensione = new RecensioneBean(val,commento,idP,idAtt);
+        try {
+			rd.doSave(recensione);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        AttivitaDAO adao=new AttivitaDAO();
+        AttivitaBean a =new AttivitaBean();
+		a=adao.doRetrieveByKey(idAtt);
+		request.setAttribute("singolaAttivita", a);
+		request.setAttribute("recensioneFatta", true);
+        request.getRequestDispatcher("/attivitaSpecifica.jsp").forward(request,response);
+		}
+        
+	}
+
+}
---
+++ wheredoieat v2.0/src/server/ServletSuggerimenti.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.PersonaBean;
+import bean.SuggerimentiBean;
+import dao.SuggerimentiDAO;
+
+/**
+ * Servlet implementation class ServletSuggerimenti
+ */
+@WebServlet("/ServletSuggerimenti")
+public class ServletSuggerimenti extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSuggerimenti() {
+        super();
+        // TODO Auto-generated constructor stub 
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	     String titolo= request.getParameter("titolo");
+	     String descrizione= request.getParameter("descrizione");
+	     SuggerimentiDAO sdao= new SuggerimentiDAO();
+	     SuggerimentiBean s=new SuggerimentiBean();
+	     PersonaBean p=(PersonaBean) request.getSession().getAttribute("cliente");
+	     String personaUsername=p.getUsername();
+	     s.setTesto(descrizione);
+	     s.setTitolo(titolo);
+	     s.setPersonaUsername(personaUsername);
+	     try {
+			sdao.doSave(s);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     request.setAttribute("done", true);
+         RequestDispatcher rd=request.getRequestDispatcher("suggerimenti.jsp");
+         rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
